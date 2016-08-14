6e5bd7a99e6be2dfb639034d397451d0038360fd
kobyov <kobyov@gmail.com>
2016-08-14 15:27:13 +1200

    added mountpoints support to Hull

--- shipgen.js
+++ shipgen.js
@@ -1,20 +1,37 @@
-var Hull = function (type, name) {
+var Hull = function (name, type, mountpoints, space) {
     this.name = name;
     this.type = type;
-    this.mountpoints = null;
-    this.weapons = null;
+    this.mountpoints = {
+        prow: mountpoints.prow || 0,
+        dorsal: mountpoints.dorsal || 0,
+        port: mountpoints.port || 0,
+        starboard: mountpoints.starboard || 0,
+        keel: mountpoints.keel || 0
+    };
+    this.space = null;
+};
+
+var hulls = {
+    //dauntless: "Dauntless"
+    dauntless: new Hull("Dauntless", "Light Cruiser", {prow: 1, port: 2, starboard: 2}, 50)
 };
 
 var Ship = function (name, type, hull) {
     this.name = name;
     this.type = type;
-    this.hull = new Hull(this.type, hull);
+    this.hull = hulls[hull];
     this.essential = null;
     this.supplementary = null;
     this.power_used = null;
     this.space_used = null;
 };
 
-//testing linkage of things
-var test = new Ship("Mordacity", "Light Cruiser", "Dauntless");
+//testing class linkage
+var test = new Ship("Mordacity", "Light Cruiser", "dauntless");
 console.log("Created the " + test.name + " a " + test.hull.name + " class " + test.type);
+console.log("Mountpoints available");
+console.log("prow:" + test.hull.mountpoints.prow);
+console.log("dorsal:" + test.hull.mountpoints.dorsal);
+console.log("port:" + test.hull.mountpoints.port);
+console.log("starboard:" + test.hull.mountpoints.starboard);
+console.log("keel:" + test.hull.mountpoints.keel);
